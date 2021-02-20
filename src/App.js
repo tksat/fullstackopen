bc429664af8dcdbc048ba81377ef68bb44c6476d
tksat <satoko.takada@findy.co.jp>
2021-02-20 22:34:16 +0900

    stateを個別で管理する

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
-function App() {
+import React, { useState } from 'react'
+
+const App = () => {
+  const [left, setLeft] = useState(0)
+  const [right, setRight] = useState(0)
+
   return (
-    <div className="App">
-      ベースとなるファイルです
+    <div>
+      {left}
+      <button onClick={() => setLeft(left + 1)}>left</button>
+      <button onClick={() => setRight(right + 1)}>right</button>
+      {right}
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
